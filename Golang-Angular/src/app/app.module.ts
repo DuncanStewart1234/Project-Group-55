c5d9fb24e89b5e02ab44dbb3f800c9273755d02d
Sean-UF <smcneil@ufl.edu>
2023-02-04 23:22:03 -0500

    Added Todo

--- Golang-Angular/src/app/app.module.ts
+++ Golang-Angular/src/app/app.module.ts
@@ -5,18 +5,23 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DatetimeComponent } from './datetime/datetime.component';
+import { TodoComponent } from './todo/todo.component';
 
+import { TodoService } from './todo.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatetimeComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

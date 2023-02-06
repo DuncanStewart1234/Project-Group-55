2df1d913ef693cbd5aac40d8bc73276ea29bf11a
scottbarcomb <scott.barcomb@ufl.edu>
2023-02-06 14:26:34 -0500

    Changes to main.go

--- Golang-Angular/src/server/main.go
+++ Golang-Angular/src/server/main.go
@@ -1,36 +1,48 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"path"
+	"path/filepath"
+
 	"github.com/DuncanStewart1234/Project-Group-55/Golang-Angular/src/server/handlers"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
-    r.Use(CORSMiddleware())
+	r := gin.Default()
+	r.Use(CORSMiddleware())
+	r.NoRoute(func(c *gin.Context) {
+		dir, file := path.Split(c.Request.RequestURI)
+		ext := filepath.Ext(file)
+		if file == "" || ext == "" {
+			c.File("./ui/dist/ui/index.html")
+		} else {
+			c.File("./ui/dist/ui/" + path.Join(dir, file))
+		}
+	})
 
-    r.GET("/todo", handlers.GetTodoListHandler)
-    r.POST("/todo", handlers.AddTodoHandler)
-    r.DELETE("/todo/:id", handlers.DeleteTodoHandler)
-    r.PUT("/todo", handlers.CompleteTodoHandler)
+	r.GET("/todo", handlers.GetTodoListHandler)
+	r.POST("/todo", handlers.AddTodoHandler)
+	r.DELETE("/todo/:id", handlers.DeleteTodoHandler)
+	r.PUT("/todo", handlers.CompleteTodoHandler)
 
-    err := r.Run(":3000")
-    if err != nil {
-        panic(err)
-    }
+	err := r.Run(":3000")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func CORSMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, POST, PUT")
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, POST, PUT")
 
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
-            return
-        }
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 
-        c.Next()
-    }
+		c.Next()
+	}
 }

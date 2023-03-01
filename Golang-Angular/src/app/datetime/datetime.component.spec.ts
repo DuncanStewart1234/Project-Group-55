6fa6cd0fe4dc62d9ca071c00674cbd2f86391df8
DuncanStewart1234 <73536076+DuncanStewart1234@users.noreply.github.com>
2023-03-01 15:02:52 -0500

    Update datetime.component.spec.ts

--- Golang-Angular/src/app/datetime/datetime.component.spec.ts
+++ Golang-Angular/src/app/datetime/datetime.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { DateTimeComponent } from './todo.component';
+import { DatetimeComponent } from './todo.component';
 
 describe('DatetimeComponent', () => {
   let component: DatetimeComponent;

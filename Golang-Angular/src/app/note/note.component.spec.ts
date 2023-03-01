f56b5b9e52d7735eb28a1ad425f6db37db098578
DuncanStewart1234 <73536076+DuncanStewart1234@users.noreply.github.com>
2023-03-01 15:05:44 -0500

    Update note.component.spec.ts

--- Golang-Angular/src/app/note/note.component.spec.ts
+++ Golang-Angular/src/app/note/note.component.spec.ts
@@ -1,27 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Note } from './todo.component';
+import { NoteComponent } from './note.component';
 
-describe('Note', () => {
-  let component: Note;
-  let fixture: ComponentFixture<Note>;
+describe('NoteComponent', () => {
+  let component: NoteComponent;
+  let fixture: ComponentFixture<NoteComponent>;
 
   beforeEach(async(() => {
    TestBed.configureTestingModule({
       declarations: [
-         Note
+         NoteComponent
       ],
    }).compileComponents();
 }));
 
   it('component should be created', async(() => {
-    const fixture = TestBed.createComponent(Note);
+    const fixture = TestBed.createComponent(NoteComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 }));
 
   it(`component should have title 'app-note'`, async(() => {
-     const fixture = TestBed.createComponent(Note);
+     const fixture = TestBed.createComponent(NoteComponent);
      const app = fixture.debugElement.componentInstance;
      expect(app.title).toEqual('app-note');
 }));

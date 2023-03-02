552c5920b123300bbbe96cf2947b9582686ed0d7
DuncanStewart1234 <73536076+DuncanStewart1234@users.noreply.github.com>
2023-03-01 21:39:12 -0500

    Update note.component.spec.ts

--- Golang-Angular/src/app/note/note.component.spec.ts
+++ Golang-Angular/src/app/note/note.component.spec.ts
@@ -1,28 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { NoteComponent } from './note.component';
+import { NoteService } from '../note.service';
+
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 describe('NoteComponent', () => {
   let component: NoteComponent;
   let fixture: ComponentFixture<NoteComponent>;
 
-  beforeEach(async(() => {
-   TestBed.configureTestingModule({
-      declarations: [
-         NoteComponent
-      ],
-   }).compileComponents();
-}));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NoteComponent ],
+      imports: [HttpClientModule, FormsModule, BrowserModule],
+      providers: [NoteService]
+    })
+    .compileComponents();
 
-  it('component should be created', async(() => {
-    const fixture = TestBed.createComponent(NoteComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    fixture = TestBed.createComponent(NoteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
-}));
 
-  it(`component should have title 'app-note'`, async(() => {
-     const fixture = TestBed.createComponent(NoteComponent);
-     const app = fixture.debugElement.componentInstance;
-     expect(app.title).toEqual('app-note');
-}));
-
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

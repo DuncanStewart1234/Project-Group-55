fa11ee26d74d1e63a303731845e234cd47f62ff8
DuncanStewart1234 <73536076+DuncanStewart1234@users.noreply.github.com>
2023-03-01 21:54:50 -0500

    Update note.service.spec.ts

--- Golang-Angular/src/app/note.service.spec.ts
+++ Golang-Angular/src/app/note.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NoteService } from './note.service';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 describe('NoteService', () => {
   let service: NoteService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, FormsModule, BrowserModule],
+      providers: [NoteService]
+    });
     service = TestBed.inject(NoteService);
   });
 

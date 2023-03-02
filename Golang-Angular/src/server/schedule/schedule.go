878d8b7582cd0fb901bc12789f0fd4e9947c92fb
scottbarcomb <scott.barcomb@ufl.edu>
2023-03-01 20:35:27 -0500

    Add files via upload

--- Golang-Angular/src/server/schedule/schedule.go
+++ Golang-Angular/src/server/schedule/schedule.go
@@ -1,53 +1,61 @@
+// The schedule package implements and maintains a schedule of classes for a student
 package schedule
 
 import (
 	"errors"
 	"sync"
-    "gorm.io/gorm"
 
-    "github.com/rs/xid"
+	"gorm.io/gorm"
+
 	"github.com/DuncanStewart1234/Project-Group-55/Golang-Angular/src/server/utils"
+	"github.com/rs/xid"
 )
 
 var (
 	list []StudentSchedule
-	db *gorm.DB
+	db   *gorm.DB
 	mtx  sync.RWMutex
 	once sync.Once
 )
 
+// StudentSchedule is the struct used to create a class schedule
 type StudentSchedule struct {
 	gorm.Model
 	ID       string `json:"id"`
-	User_ID	 string `json:"uid"`
+	User_ID  string `json:"uid"`
 	Class_ID string `json:"cid"`
 }
 
+// init is a constructor, calls initialiseList
 func init() {
 	once.Do(initialiseList)
 }
 
+// initialiseList creates the schedule array and calls initDatabase
 func initialiseList() {
 	list = []StudentSchedule{}
 	initDatabase()
 }
 
+// initDatabase initialises the database
 func initDatabase() {
-	db = utils.GetDB("src/server/databases/schedules.db")
+	db = utils.GetDB("C:/Users/scott/go/src/github.com/DuncanStewart1234/Project-Group-55/Golang-Angular/src/server/databases/schedules.db")
 
-    db.AutoMigrate(&StudentSchedule{})
+	db.AutoMigrate(&StudentSchedule{})
 
 	// TODO: Get only curr user elements
 	result := db.Find(&list)
-    if result.Error	!= nil {
-        panic("failed to connect database")
-    }
+	if result.Error != nil {
+		panic("failed to connect database")
+	}
 }
 
+// Get returns the schedule
 func Get() []StudentSchedule {
 	return list
 }
 
+// Add creates and adds a class to the schedule
 func Add(cid string) (string, error) {
 	t := newStudentSchedule(cid)
 	if cid == "" {
@@ -60,6 +68,7 @@ func Add(cid string) (string, error) {
 	return t.ID, nil
 }
 
+// Delete removes and deletes a class from the schedule
 func Delete(id string) error {
 	location, err := findStudentScheduleLocation(id)
 	if err != nil {
@@ -71,15 +80,17 @@ func Delete(id string) error {
 	return nil
 }
 
+// newStudentSchedule is a helper function to Add
 func newStudentSchedule(cid string) StudentSchedule {
 	// TODO: Get UID
-	return StudentSchedule {
+	return StudentSchedule{
 		ID:       xid.New().String(),
-		User_ID: "1005",
-		Class_ID:  cid,
+		User_ID:  "1005",
+		Class_ID: cid,
 	}
 }
 
+// findStudentScheduleLocation is a helper function to Delete
 func findStudentScheduleLocation(id string) (int, error) {
 	mtx.RLock()
 	defer mtx.RUnlock()
@@ -91,6 +102,7 @@ func findStudentScheduleLocation(id string) (int, error) {
 	return 0, errors.New("could not find schedule based on id")
 }
 
+// removeElementByLocation is a helper function to Delete
 func removeElementByLocation(i int) {
 	mtx.Lock()
 	list = append(list[:i], list[i+1:]...)

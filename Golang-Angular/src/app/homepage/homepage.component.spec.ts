7bac2e2e7b866c6d7c9b66ee0501b9d4ff4f8753
DuncanStewart1234 <73536076+DuncanStewart1234@users.noreply.github.com>
2023-04-19 22:31:19 -0400

    Update homepage.component.spec.ts

--- Golang-Angular/src/app/homepage/homepage.component.spec.ts
+++ Golang-Angular/src/app/homepage/homepage.component.spec.ts
@@ -2,13 +2,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomepageComponent } from './homepage.component';
 
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 describe('HomepageComponent', () => {
   let component: HomepageComponent;
   let fixture: ComponentFixture<HomepageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomepageComponent ]
+      declarations: [ HomepageComponent ],
+      imports: [HttpClientModule, FormsModule, BrowserModule],
+      providers: [TodoService]
     })
     .compileComponents();
 

4437da3cd546f0d91bd0ac0704d35dd88c75593a
DuncanStewart1234 <73536076+DuncanStewart1234@users.noreply.github.com>
2023-04-19 22:26:55 -0400

    Update login.service.spec.ts

--- Golang-Angular/src/app/login.service.spec.ts
+++ Golang-Angular/src/app/login.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoginService } from './login.service';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 describe('LoginService', () => {
   let service: LoginService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClientModule, FormsModule, BrowserModule],
+      providers: [LoginService]});
     service = TestBed.inject(LoginService);
   });
 

84f01347a0e5d086427792356ea6274f046919da
DuncanStewart1234 <73536076+DuncanStewart1234@users.noreply.github.com>
2023-04-19 22:33:34 -0400

    Update login.component.spec.ts

--- Golang-Angular/src/app/login/login.component.spec.ts
+++ Golang-Angular/src/app/login/login.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { LoginService } from '../login.service';
 import { LoginComponent } from './login.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +13,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [HttpClientModule, FormsModule, BrowserModule],
+      providers: [LoginService]
     })
     .compileComponents();
 
